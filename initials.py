5d87d4f402e0bb1a05d96bfc8ceef8a97b40ffc0
nkdoris <123142158+nkdoris@users.noreply.github.com>
2026-05-30 12:44:04 +0000

    Submission of my assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
 
+
 def main():
-  pass #code goes here
+#This is where I start coding my credentials and initials
+    print("#File: initials.py")
+    print("#Description: A program written to display my initials ")
+    print("#Assignment number:2")
+    print("#")
+    print("#Name : Doris Kai Adjei")
+    print("#STUDENT ID: 2425402168")
+    print("#Email : 2425402168@live.gctu.edu.gh")
+    print("# Grader: Augustus Buckman")
+    print("#")
+    print("#On my honour, Doris Kai Adjei,this programming assignment is my own work")
+    print("#and I have not provided this code to any other student.")
 
-
+    print("")
+    print("...DKA")
+    print("")
+    print("...DDD.................KKK.....KKK.............AAAA.........")
+    print("...DDDDD...............KKK....KKK.............AA..AA........")
+    print("...DDD..DDD............KKK...KKK.............AAA...AAA......")
+    print("...DDD...DDD...........KKK..KKK.............AAA.....AAA.....")
+    print("...DDD....DDD..........KKK.KKK..............AAAAAAAAAAA.....")
+    print("...DDD.....DDD.........KKKK.KKK.............AAA.....AAA.....")
+    print("...DDD......DDD........KKK...KKK............AAA.....AAA.....")
+    print("...DDD......DDD........KKK....KKK...........AAA.....AAA.....")
+    print("...DDDDDDDDDDDD...**...KKK.....KKK....**....AAA.....AAA...**")
+    print("...DDDDDDDDDDDD...**...KKK......KKK...**....AAA.....AAA...**")
+    print("")
 
 main()
+
+
